63a74a441591cae774438d2efd832bf620a75d67
ShrutiDiwan1403 <shrutisindhi1403@gmail.com>
2022-04-16 00:17:00 +0100

    Changes in init file

--- Auth/_initial_.py
+++ Auth/_initial_.py
@@ -2,14 +2,14 @@ import pyrebase
 
 # Configuration
 config = {
-  "apiKey": "",
-  "authDomain": "",
-  "databaseURL": "",
-  "projectId": "",
-  "storageBucket": "",
-  "messagingSenderId": "",
-  "appId": "",
-  "measurementId": ""
+  "apiKey": "AIzaSyDvqkK0qG_Mhdh2AgMwZcWNUfIcsvpHZCU",
+  "authDomain": "assignment2-21973.firebaseapp.com",
+  "databaseURL": "https://assignment2-21973-default-rtdb.firebaseio.com",
+  "projectId": "assignment2-21973",
+  "storageBucket": "assignment2-21973.appspot.com",
+  "messagingSenderId": "625821177645",
+  "appId": "1:625821177645:web:ecf64eb22a9ea7a619e5a4",
+  #"measurementId": ""
 }
 
 # Initialize firebase
