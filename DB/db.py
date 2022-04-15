1e82ea021a9803bcf945b2822b3e96e061193018
ShrutiDiwan1403 <shrutisindhi1403@gmail.com>
2022-04-15 01:42:06 +0100

    Changes in a python file

--- DB/db.py
+++ DB/db.py
@@ -3,4 +3,4 @@ from google.cloud import datastore
 
 
 os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-client = datastore.Client("alien-grove-346709")  # os.getenv("DATASTORE_CLIENT")
+client = datastore.Client("alien-grove-346709") 
