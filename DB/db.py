4518cdf8e901a3d506bfd754cfbcf320b15c6c7a
ShrutiDiwan1403 <shrutisindhi1403@gmail.com>
2022-04-16 00:28:18 +0100

    Changes i the db file

--- DB/db.py
+++ DB/db.py
@@ -1,6 +1,6 @@
 import os
 from google.cloud import datastore
 
-
+# export GOOGLE_APPLICATION_CREDENTIALS="assignment2-21973-f1f7c792ead9.json"
 os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-client = datastore.Client("alien-grove-346709") 
+client = datastore.Client("alien-grove-346709")   # os.getenv("DATASTORE_CLIENT")
