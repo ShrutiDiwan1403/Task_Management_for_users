ee20c0dc13bb08a2927ad1c1838924187cc59ad5
ShrutiDiwan1403 <shrutisindhi1403@gmail.com>
2022-04-17 16:22:19 +0100

    Fixing the problem

--- DB/__init__.py
+++ DB/__init__.py
@@ -4,4 +4,4 @@ from google.cloud import datastore
 # export GOOGLE_APPLICATION_CREDENTIALS="assignment2-21973-f1f7c792ead9.json"
 os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 #GOOGLE_APPLICATION_CREDENTIALS="..\assignment2-21973-f1f7c792ead9.json"
-client = datastore.Client("alien-grove-346709")   # os.getenv("DATASTORE_CLIENT")
+client = datastore.Client("prime-rainfall-340817") # os.getenv("DATASTORE_CLIENT")
